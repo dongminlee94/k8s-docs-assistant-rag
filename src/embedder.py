09f8bb80fa5b64b70d44dc999e0603498b6046a4
dongminlee94 <kid33629@gmail.com>
2024-08-05 20:15:25 +0900

    embedder draft 2

--- src/embedder.py
+++ src/embedder.py
@@ -1,42 +1,65 @@
 """Embedder."""
 
 import os
-from typing import Any
 
 import pandas as pd
 import tiktoken
 
-df = pd.read_parquet(os.path.join(os.path.dirname(__file__), "..", "data/summary.parquet"))
+from client import OpenAIClient
+
+summary_df = pd.read_parquet(os.path.join(os.path.dirname(__file__), "..", "data/summary.parquet"))
 
 embedding_model = "text-embedding-3-large"
 encoding_name = "cl100k_base"
 max_tokens = 8192
 
-df["embedding_input"] = df["title"] + " " + df["content"]
+summary_df["embedding_input"] = summary_df["title"] + " " + summary_df["content"]
 
-columns = ["title", "url", "content", "summary", "embedding_input"]
-new_df = pd.DataFrame(columns=columns)
+columns = ["title", "url", "content", "summary", "embedding_input", "embedding_output"]
+rows = []
 
 
-def make_chunks(data: str | list[Any], length: int) -> list[str]:
+def make_chunks(data: str | list[int], length: int) -> list[str]:
     return [data[i : i + length] for i in range(0, len(data), length)]
 
 
 encoding = tiktoken.get_encoding(encoding_name=encoding_name)
 
-for idx, row in df.iterrows():
+for idx, row in summary_df.iterrows():
     embedding_input = row["embedding_input"]
     tokens = encoding.encode(text=embedding_input)
 
     for chunk in make_chunks(data=tokens, length=max_tokens):
         text = encoding.decode(tokens=chunk)
 
+        rows.append(
+            {
+                "title": row["title"],
+                "url": row["url"],
+                "content": row["content"],
+                "summary": row["summary"],
+                "embedding_input": text,
+                "embedding_output": None,
+            },
+        )
 
-# with open(os.path.join(os.path.dirname(__file__), "..", "env/api_key.env"), "r") as file:
-#     api_key = file.read().strip()
+embedding_df = pd.DataFrame(data=rows, columns=columns)
 
-# client = OpenAIClient(api_key=api_key)
+embedding_df
 
-# result = client.create_embedding(text=text_8192, model="text-embedding-3-large")
+print(embedding_df.shape)
+print(embedding_df.columns)
+print(embedding_df.head())
 
-# print(len(result))
+with open(os.path.join(os.path.dirname(__file__), "..", "env/api_key.env"), "r") as file:
+    api_key = file.read().strip()
+
+client = OpenAIClient(api_key=api_key)
+
+for idx, row in embedding_df.iterrows():
+    embedding_output = client.create_embedding(text=row["embedding_input"], model=embedding_model)
+    embedding_df.loc[idx, "embedding_output"] = embedding_output
+
+print(embedding_df.shape)
+print(embedding_df.columns)
+print(embedding_df.head())
