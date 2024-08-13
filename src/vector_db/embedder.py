7e77d1d3155f0ccf48a8ed5767147a021b47120c
dongminlee94 <kid33629@gmail.com>
2024-08-13 13:24:22 +0900

    fix file path in embedder module

--- src/vector_db/embedder.py
+++ src/vector_db/embedder.py
@@ -24,8 +24,8 @@ class DocsEmbedder:
         self._openai_client = OpenAI(api_key=api_key)
         self._target_subdirs = target_subdirs
 
-        self._input_dir = os.path.join(os.path.dirname(__file__), "../../..", "data/docs")
-        self._output_path = os.path.join(os.path.dirname(__file__), "../../..", "data/vector_db.parquet")
+        self._input_dir = os.path.join(os.path.dirname(__file__), "../..", "data/docs")
+        self._output_path = os.path.join(os.path.dirname(__file__), "../..", "data/vector_db.parquet")
 
         self._base_columns = [
             "title",
@@ -55,7 +55,7 @@ class DocsEmbedder:
                     if df["url"].item() not in embedded_urls:
                         dfs.append(df)
 
-            return pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
+            return pd.concat(objs=dfs, ignore_index=True) if dfs else pd.DataFrame(columns=self._base_columns)
 
         return __read_docs(input_dir=self._input_dir)
 
@@ -90,7 +90,7 @@ class DocsEmbedder:
             embedded_df = pd.read_parquet(self._output_path)
             embedded_urls = set(embedded_df["url"].unique())
         else:
-            embedded_df = pd.DataFrame(columns=self._base_columns[:-2])
+            embedded_df = pd.DataFrame(columns=self._base_columns)
             embedded_urls = set()
 
         unembedded_df = self._read_docs(embedded_urls=embedded_urls)
@@ -165,7 +165,7 @@ class DocsEmbedder:
         """
         embedded_df, unembedded_df = self._get_embedding_input(model=model, max_tokens=max_tokens)
         new_embedded_df = self._get_embedding_output(
-            embedded_df=embedded_df, unembedded_df=unembedded_df, model=model, verbose=verbose
+            unembedded_df=unembedded_df, model=model, verbose=verbose
         )
 
         vector_db_df = pd.concat([embedded_df, new_embedded_df], ignore_index=True)
