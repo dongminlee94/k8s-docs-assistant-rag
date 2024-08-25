deea631cb2310f0694469f633a660752abca66e1
dongminlee94 <kid33629@gmail.com>
2024-08-25 23:26:00 +0900

    change subdirs to subdomains

--- src/vector_db/crawler.py
+++ src/vector_db/crawler.py
@@ -17,15 +17,15 @@ class K8sDocsCrawler:
     This class is used to crawl and download documentation from the Kubernetes website.
     """
 
-    def __init__(self, url: str, target_subdirs: list[str]) -> None:
+    def __init__(self, url: str, target_subdomains: list[str]) -> None:
         """
         Initialize the K8sDocsCrawler.
 
         :param url: The base URL of the Kubernetes documentation.
-        :param target_subdirs: List of target subdirectories to process.
+        :param target_subdomains: List of target subdomains to process.
         """
         self._url = url
-        self._target_subdirs = target_subdirs
+        self._target_subdomains = target_subdomains
         self._base_url = self._get_base_url()
 
     def _requests_retry_session(
@@ -89,7 +89,7 @@ class K8sDocsCrawler:
         for a_tag in soup.find_all("a", href=True):
             href = a_tag["href"]
 
-            if href.startswith("/docs/") and href.split("/")[2] in self._target_subdirs:
+            if href.startswith("/docs/") and href.split("/")[2] in self._target_subdomains:
                 urls.append(self._base_url + href)
 
         return list(set(urls))
